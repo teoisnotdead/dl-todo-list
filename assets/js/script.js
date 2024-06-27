a8aac54afb46428806454a5167248b4290b0effa
TEO <alfredo.ssm@gmail.com>
2024-06-27 19:02:34 -0400

    Se crea objeto task donde se almacenaran las tareas, se agrega archivo script

---
+++ assets/js/script.js
@@ -0,0 +1,19 @@
+const tasks = [
+  {
+    id: 1,
+    name: 'Tarea 1',
+    completed: false
+  },
+  {
+    id: 2,
+    name: 'Tarea 2',
+    completed: false
+  },
+  {
+    id: 3,
+    name: 'Tarea 3',
+    completed: false
+  }
+]
+
+console.log(tasks);
