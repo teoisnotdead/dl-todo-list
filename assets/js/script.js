c53824c708317a05f4747063fef1ba703a991d6d
TEO <alfredo.ssm@gmail.com>
2024-06-27 21:23:29 -0400

    Se crea createTask para mostrar las tareas

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,19 +7,41 @@ const tasks = [
   {
     id: 2,
     name: 'Tarea 2',
-    completed: false
+    completed: true
   },
   {
     id: 3,
+    name: 'Tarea 3',
+    completed: true
+  }
+  ,
+  {
+    id: 4,
     name: 'Tarea 3',
     completed: false
   }
 ]
 
-const inputTask = document.querySelector('#task');
-const btnAddTask = document.querySelector('#add');
-const toDoList = document.querySelector('#todo-list');
+const inputTask = document.querySelector('#task')
+const btnAddTask = document.querySelector('#add')
+const toDoList = document.querySelector('#todo-list')
 
-console.log(inputTask);
-console.log(btnAddTask);
-console.log(toDoList);
+const createTask = (taskList) => {
+  taskList.forEach((task) => {
+    const { id, name, completed } = task
+    const btnBgCompleted = `${completed ? 'bg-yellow-500 hover:bg-yellow-400' : 'bg-green-500 hover:bg-green-400'}`
+    const liBgCompleted = `${completed ? 'bg-green-200' : ''}`
+    const btnText = `${completed ? 'Dejar pendiente' : 'Completar'}`
+
+    const li = document.createElement('li')
+    li.className = `border border-gray-300 p-2 mt-2 flex justify-between ${liBgCompleted}`
+    li.innerHTML = `<span><strong>ID: ${id}</strong> ${name}</span>
+                    <div id="actions" class="flex justify-end gap-2">
+                        <button class="${btnBgCompleted} text-white py-1 px-2 rounded">${btnText}</button>
+                        <button class="bg-red-500 text-white py-1 px-2 hover:bg-red-400 rounded">Eliminar</button>
+                    </div>`
+    toDoList.appendChild(li)
+  })
+}
+
+createTask(tasks)
