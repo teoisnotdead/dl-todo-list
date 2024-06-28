54eba437fe474d9ab6505dfde0abfd56372e8352
TEO <alfredo.ssm@gmail.com>
2024-06-28 00:06:10 -0400

    Se crea completeTask y deleteTask

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,32 +1,40 @@
+const generateId = () => {
+  return `${Date.now()}${Math.floor(Math.random() * 1000)}`
+}
+
 const tasks = [
   {
-    id: 1,
+    id: generateId(),
     name: 'Tarea 1',
     completed: false
   },
   {
-    id: 2,
+    id: generateId(),
     name: 'Tarea 2',
     completed: true
   },
   {
-    id: 3,
+    id: generateId(),
     name: 'Tarea 3',
     completed: true
   }
   ,
   {
-    id: 4,
+    id: generateId(),
     name: 'Tarea 3',
     completed: false
   }
 ]
 
+
+
 const inputTask = document.querySelector('#task')
 const btnAddTask = document.querySelector('#add')
 const toDoList = document.querySelector('#todo-list')
+const totalTasks = document.querySelector('#total')
+const completedTasks = document.querySelector('#completed')
 
-const createTask = (taskList) => {
+const renderToDoList = (taskList) => {
   taskList.forEach((task) => {
     const { id, name, completed } = task
     const btnBgCompleted = `${completed ? 'bg-yellow-500 hover:bg-yellow-400' : 'bg-green-500 hover:bg-green-400'}`
@@ -35,13 +43,67 @@ const createTask = (taskList) => {
 
     const li = document.createElement('li')
     li.className = `border border-gray-300 p-2 mt-2 flex justify-between ${liBgCompleted}`
-    li.innerHTML = `<span><strong>ID: ${id}</strong> ${name}</span>
+    li.innerHTML = `<span><strong>ID: ${id}</strong> - ${name}</span>
                     <div id="actions" class="flex justify-end gap-2">
                         <button class="${btnBgCompleted} text-white py-1 px-2 rounded">${btnText}</button>
                         <button class="bg-red-500 text-white py-1 px-2 hover:bg-red-400 rounded">Eliminar</button>
                     </div>`
     toDoList.appendChild(li)
   })
+
+  totalTasks.textContent = taskList.length
+  completedTasks.textContent = taskList.filter(task => task.completed).length
+  inputTask.value = ''
 }
 
-createTask(tasks)
+const addTask = () => {
+  const task = {
+    id: generateId(),
+    name: inputTask.value,
+    completed: false
+  }
+
+  tasks.push(task)
+  toDoList.innerHTML = ''
+  renderToDoList(tasks)
+}
+
+const completeTask = (id) => {
+  console.log('id', id);
+  console.log('tasks', tasks);
+  const task = tasks.find(task => task.id === id)
+  console.log('task', task);
+  task.completed = !task.completed
+  toDoList.innerHTML = ''
+  renderToDoList(tasks)
+}
+
+const deleteTask = (id) => {
+  const index = tasks.findIndex(task => task.id === id)
+  tasks.splice(index, 1)
+  toDoList.innerHTML = ''
+  renderToDoList(tasks)
+}
+
+btnAddTask.addEventListener('click', addTask)
+
+toDoList.addEventListener('click', (e) => {
+  console.log('e', e.target)
+  const target = e.target
+  const parent = target.parentElement.parentElement
+  console.log('parent', parent);
+  const id = parent.querySelector('strong').textContent.split(' ')[1]
+
+  console.log('id1', id);
+  if (target.textContent === 'Completar' || target.textContent === 'Dejar pendiente') {
+    console.log('target', target.textContent);
+    completeTask(id)
+  }
+
+  if (target.textContent === 'Eliminar') {
+    deleteTask(id)
+  }
+
+})
+
+renderToDoList(tasks)
